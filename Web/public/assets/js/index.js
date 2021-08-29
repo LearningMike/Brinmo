b48b6395eea2caadb7b119cbab9507883c0381f4
Michael Abia <michael.abia@aun.edu.ng>
2021-08-29 23:35:06 +0100

    cleaning up again

--- Web/public/assets/js/index.js
+++
@@ -1,9 +0,0 @@
-// get charge
-var bcharge = document.getElementById("bcharge");
-var ccharge = document.getElementById("ccharge");
-var mval = document.getElementById("mval");
-var amnt = document.getElementById("amnt");
-function getCharge(){
-	amnt.innerText = "₦"+mval.value;
-	bcharge.innerText = "₦" + ((mval.value / 100) * 4);
-}
